ad9872618e935171a6e0d0e69c237dc10ec0deec
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-02-13 17:56:43 +0300

    yasuo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function App() {
-  return <div>
-    yasuo
-    <p className='yasuo'>1</p>
-    <p className='yasuo'>2</p>
-    <p className='yasuo'>3</p>
-  </div>;
+  return <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+  </ul>;
 }
 
 export default App;
