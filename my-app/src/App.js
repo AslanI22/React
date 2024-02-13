4e8b37778e1f480693144ad67ee36a23e0160a2f
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-02-13 17:51:38 +0300

    Configure line endings for JavaScript files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return <div>
-    text
+    yasuo
+    <p className='yasuo'>1</p>
+    <p className='yasuo'>2</p>
+    <p className='yasuo'>3</p>
   </div>;
 }
 
