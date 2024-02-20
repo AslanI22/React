322b69a69ffd1833051ed9fdb3e14ccc0141b58e
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-02-20 17:40:00 +0300

    yasuo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 
 function App() {
-
-  function getDigitsSum(number) {
-    let sum = 0;
-    let num = number;
-    while (num !== 0) {
-      let last = num % 10;
-      sum += last;
-      num = Math.floor(num / 10);
-    }
-    return sum;
+  function show1() {
+    alert(1);
   }
 
-  const x = getDigitsSum(123);
+  function show2() {
+    alert(2);
+  }
 
-  return (
-    <div>
-      <p>{x}</p>
-    </div>
-  );
+  return <div>
+    <button onClick={show1}>show</button>
+    <button onClick={show2}>show</button>
+  </div>;
 }
 
 export default App;
