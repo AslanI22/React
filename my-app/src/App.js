270035145b9b1c819626dc4b272a34ab46633534
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-02-20 16:35:40 +0300

    yasuo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,26 @@
 import React from 'react';
 
 function App() {
-  return <div>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-    </ul>
-  </div>;
-  //   return <table>
-  //   <tbody>
-  //     <tr>
-  //       <td>1</td>
-  //       <td>2</td>
-  //       <td>3</td>
-  //     </tr>
-  //     <tr>
-  //       <td>4</td>
-  //       <td>5</td>
-  //       <td>6</td>
-  //     </tr>
-  //     <tr>
-  //       <td>7</td>
-  //       <td>8</td>
-  //       <td>9</td>
-  //     </tr>
-  //   </tbody>
-  // </table>;
-}
+  const num1 = 3;
+  const num2 = 2;
 
+  return <div>
+    result: {num1 ** num2}
+  </div>;
+}
+// function App() {
+//   const name = 'john';
+//   const surname = 'smit';
+
+//   return <div>
+//     result: {name + ' ' + surname}
+//   </div>;
+// }
+// function App() {
+// 	const num = 4;
+
+// 	return <div>
+// 		result: {Math.sqrt(num)}
+// 	</div>;
+// }
 export default App;
