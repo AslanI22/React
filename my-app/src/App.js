01a727ef43b53dff6b70f276f3b42cc3ce65e6ed
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-02-27 16:30:40 +0300

    yasuo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function App() {
   function func(event) {
-    event = 10;
-    console.log(event); // объект с событием
+    // event = 10;
+    // console.log(event);
+    console.log(event.target);
   }
 
   return <div>
