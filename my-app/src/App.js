79be1bf6675c9a0f1cd3fbf58002b7980970adca
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-03-05 16:37:50 +0300

    yasuo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { nanoid } from 'nanoid'
 import uuid from 'react-uuid';
 
+const prods = [
+  { id: id(), name: 'product1', cost: 100 },
+  { id: id(), name: 'product2', cost: 200 },
+  { id: id(), name: 'product3', cost: 300 },
+];
+
+
+function id() {
+  let x = nanoid();
+  return x
+}
 function App() {
-  function id() {
-    const x = nanoid();
-    const y = uuid();
-    return x, y
-  }
-  return (
-    id()
-  );
+
 }
 
 export default App;
