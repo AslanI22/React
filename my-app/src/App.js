ddb2d74bc5d5bf9630299841cedd31936f1a7cd4
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-03-12 17:40:21 +0300

    yasuo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [inputText, setInputText] = useState('');
+  const [fahrenheit, setFahrenheit] = useState('');
 
   function handleChange(event) {
-    const inputValue = event.target.value;
-    setInputText(inputValue);
+    setFahrenheit(event.target.value);
+  }
+
+  function convertToCelsius() {
+    const fahrenheitValue = parseFloat(fahrenheit);
+    const celsiusValue = (fahrenheitValue - 32) * (5 / 9);
+    return isNaN(celsiusValue) ? 'Invalid input' : celsiusValue.toFixed(2);
   }
 
   return (
     <div>
-      <label>Input: </label>
-      <input value={inputText} onChange={handleChange} />
-      <p>Number of characters: {inputText.length}</p>
+      <label>Enter Fahrenheit: </label>
+      <input value={fahrenheit} onChange={handleChange} />
+      <p>Celsius: {convertToCelsius()}</p>
     </div>
   );
 }
